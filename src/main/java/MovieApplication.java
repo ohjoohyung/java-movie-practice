ed8558650fc7003c7b921a64a0d160a90bfa0b75
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2020-12-16 14:22:16 +0900

    first commit

---
+++ src/main/java/MovieApplication.java
@@ -0,0 +1,17 @@
+import domain.Movie;
+import domain.MovieRepository;
+import view.InputView;
+import view.OutputView;
+
+import java.util.List;
+
+public class MovieApplication {
+    public static void main(String[] args) {
+        List<Movie> movies = MovieRepository.getMovies();
+        OutputView.printMovies(movies);
+
+        int movieId = InputView.inputMovieId();
+
+        // TODO 구현 진행
+    }
+}
